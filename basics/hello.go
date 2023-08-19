45700e2f0587ecfdc76039ae04b30c3960db28e7
Saurabh Ahuja <sauraahu@gmail.com>
2023-08-19 10:44:09 +0530

    hello world basics

---
+++ basics/hello.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var a int
+
+func main() {
+
+	fmt.Println(a)
+	var i int = 1
+	var w float64 = 12.5
+
+	fmt.Println(i, w)
+
+	a := 30
+
+	// taking a string variable
+	Language := "Go Programming"
+
+	fmt.Println("The Value of a is: ", a)
+	fmt.Println("The Value of Language is: ", Language)
+
+}
